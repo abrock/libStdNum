4da25b12b5abda3734ec3bfca774aa2f1baa5c10
Alexander Brock <alexander@lunar-orbit.de>
2016-09-10 16:55:17 +0200

    Initial commit

---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef LIB_STD_NUM_MAIN_H
+#define LIB_STD_NUM_MAIN_H
+
+template<class C1, class C2>
+double l2sqdiff(const C1& a, const C2& b) {
+
+    return 0;
+}
+
+#endif // LIB_STD_NUM_MAIN_H
