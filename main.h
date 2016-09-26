87ade79c1170e890d7cfd226360cb139d48e7fcf
Alexander Brock <alexander@lunar-orbit.de>
2016-09-26 16:42:48 +0200

    +shuffling, test if container elements are weakly decreasing

--- main.h
+++ main.h
@@ -2,6 +2,10 @@
 #define LIB_STD_NUM_MAIN_H
 
 #include <cmath>
+#include <random>
+#include <vector>
+#include <algorithm>
+#include <chrono>
 
 namespace stdnum {
 
@@ -27,6 +31,41 @@ template<class C1, class C2>
 double l2diff(const C1& a, const C2& b) {
     return std::sqrt(l2sqdiff(a,b));
 }
+
+
+template<class Integer>
+std::vector<size_t> shuffling(const Integer size) {
+    std::vector<size_t> result;
+    result.reserve(size);
+    for (size_t ii = 0; ii < size; ++ii) {
+        result.push_back(ii);
+    }
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+
+    std::shuffle(result.begin(), result.end(), std::default_random_engine(seed));
+    return result;
+}
+
+template<class T>
+std::vector<size_t> shuffling(const std::vector<T>& c) {
+    return shuffling(c.size());
+}
+
+template<class C>
+bool is_weakly_decreasing(const C& container) {
+    if (container.empty() || container.size() < 2) {
+        return true;
+    }
+    double last = container.front();
+    for (const auto val : container) {
+        if (val > last) {
+            return false;
+        }
+        last = val;
+    }
+    return true;
+}
+
 
 } // namespace stdnum
 
